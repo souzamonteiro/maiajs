895b269892831d9378b59d58a0f0339657e065b7
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2026-07-11 19:26:01 -0300

    Fix ES8 parser coverage for templates and contextual identifiers

--- compiler/tests/ecmascript-parser-supported.test.js
+++ compiler/tests/ecmascript-parser-supported.test.js
@@ -27,7 +27,51 @@ const supportedCases = [
   { name: 'object literal with properties', source: 'const obj = {a:1, b:2};' },
   { name: 'empty object literal', source: 'const obj = {};' },
   { name: 'member call expression', source: 'x.y();' },
-  { name: 'console log call expression', source: 'console.log("hello");' }
+  { name: 'console log call expression', source: 'console.log("hello");' },
+  {
+    name: 'function argument containing template literal with computed member interpolation',
+    source: 'foo(function(entry){ bar(`x ${entry[0]}`); });'
+  },
+  {
+    name: 'template literal interpolation containing callback with block body',
+    source: 'console.log(`x ${numbers.some(function(n) { return n % 2 === 0; })}`);'
+  },
+  {
+    name: 'member access accepts reserved-word property names',
+    source: 'simplePromise.then(function(result) { console.log(`Promise then: ${result}`); }).catch(function(err) { console.error(`Promise catch: ${err}`); });'
+  },
+  {
+    name: 'function declaration accepts trailing comma in parameters',
+    source: 'function trailingCommas(param1, param2, param3,) { return param1 + param2 + param3; }'
+  },
+  {
+    name: 'computed property accepts function-valued assignment with nested object returns',
+    source: 'const range = { [Symbol.iterator]: function() { let current = this.from; return { next: function() { return { done: true }; } }; } };'
+  },
+  {
+    name: 'object literal accepts reserved-word property names before computed properties',
+    source: 'const range = { from: 1, to: 5, [Symbol.iterator]: function() { let current = this.from; const end = this.to; return { next: function() { if (current <= end) { return { value: current++, done: false }; } return { done: true }; } }; } };'
+  },
+  {
+    name: 'variable initializer accepts function expressions',
+    source: 'const expressionFunc = function(param) { return `Function expression: ${param}`; };'
+  },
+  {
+    name: 'new expression parses as constructor call statement',
+    source: 'new Foo();'
+  },
+  {
+    name: 'variable initializer accepts new expressions',
+    source: 'const genericAnimal = new Animal("Generic", "Unknown");'
+  },
+  {
+    name: 'binding identifiers accept contextual keywords like set',
+    source: 'const set = new Set([1, 2, 3]);'
+  },
+  {
+    name: 'identifier references accept contextual keywords like set',
+    source: 'set.add(5);'
+  }
 ];
 
 test('current parser accepts known-supported grammar subset', async (t) => {
